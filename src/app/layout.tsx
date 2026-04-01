90b44c9d3384713237cf78069b221f2705a73d5b
hjh <ktorn21@naver.com>
2026-04-01 22:24:02 +0900

    shadcn ui 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+import SideNavigation from "@/components/common/navigation/SideNavigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <SideNavigation />
+        {children}
+      </body>
     </html>
   );
 }
